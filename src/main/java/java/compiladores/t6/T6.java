5a6206717b49ac46c23effcee026de1f5d492a20
CassaX <matheuscassatti@estudante.ufscar.br>
2025-06-30 19:35:04 -0300

    Criação do Projeto

---
+++ src/main/java/java/compiladores/t6/T6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package java.compiladores.t6;
+
+/**
+ *
+ * @author mathe
+ */
+public class T6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
